87063d7e793fe70f17ed520994068ac96bca93fd
Arion Mehmeti <60946518+arionitas@users.noreply.github.com>
2023-12-25 18:35:13 +0000

    Linking Archive page to open up a different page

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,15 @@
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
+const navLinkEls = document.querySelectorAll(".nav__link");
+const windowPathname = window.location.pathname;
+
+navLinkEls.forEach(navLinkEl => {
+  if (navLinkEl.href.includes(windowPathname)) {
+    navLinkEl.classList.add('active');
+  }
+});
+
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
@@ -12,3 +21,5 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+
