75f200fb14640f8ffea2842ffd32ad231511d853
Arion Mehmeti <60946518+arionitas@users.noreply.github.com>
2023-12-28 21:52:12 +0000

    added backend server tracking email sign ups on emailjs

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -35,3 +35,26 @@ $('.popup-close').click(function(e) {
   $('.popup-box').removeClass('transform-in').addClass('transform-out');
   e.preventDefault();
 });
+
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_0xzmpqi";
+  const templateID = "template_0mi0cpo";
+
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your message sent successfully!!")
+
+    })
+    .catch(err=>console.log(err));
+
+}
